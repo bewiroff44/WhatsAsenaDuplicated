b4392541f6a15cea68e79aba6764ec18008e4492
Bewiroff <85064556+bewiroff44@users.noreply.github.com>
2021-08-12 01:42:45 +0400

    Create sjsj.js

---
+++ plugins/sjsj.js
@@ -0,0 +1,15 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'sj', fromMe: true}, (async (message, match) => {
+
+    if (message.reply_message === false) return await message.sendMessage('```Lütfen bir mesaja yanıt verin!```');
+
+    await message.client.sendMessage(message.jid, '' + '@' + message.reply_message.jid.split('@')[0] + ' ```Bekle Güleyim 😄```', MessageType.text, {
+        quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
+
+    });
+    await new Promise(r => setTimeout(r, 2500));
+    await message.sendMessage('┈╭━━━━━━━━━━━╮┈\n┈┃╭━━━╮┊╭━━━╮┃┈\n╭┫┃┈▇┈┃┊┃┈▇┈┃┣╮\n┃┃╰━━━╯┊╰━━━╯┃┃\n╰┫╭━╮╰━━━╯╭━╮┣╯\n┈┃┃┣┳┳┳┳┳┳┳┫┃┃┈\n┈┃┃╰┻┻┻┻┻┻┻╯┃┃┈\n┈╰━━━━━━━━━━━╯┈');
+    
+}));
