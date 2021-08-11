628cbfa8f9d2a28e4c959d6a879ae6933a0ae995
Bewiroff <85064556+bewiroff44@users.noreply.github.com>
2021-08-12 01:15:36 +0400

    Create npack.js

---
+++ plugins/npack.js
@@ -0,0 +1,64 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const NPACK = "Birbirinden farklı değişik ve eğlenceli araçların bir arada bulunduğu bir plugindir (2.versiyondur yani devamıdır)";
+   
+
+Asena.addCommand({pattern: 'npack', fromMe: true, desc: NPACK}, (async (message, match) => {
+
+    await message.sendMessage("_Birbirinden farklı değişik ve eğlenceli araçların bir arada bulunduğu bir plugin._\n💻Usage: *.m7*\nℹ️Desc: *Şekilli olarak doğum günü kutla*\n\n💻Usage: *.m8*\nℹ️Desc: *Biraz silah sıkmaya ne dersin?*\n\n💻Usage: *.m9*\nℹ️Desc: *Seni seviyorum demenin farklı bir yolu*\n\n💻Usage: *.n1*\nℹ️Desc: *Helikopterle inip aşkını açıklamaya ne dersin?*\n\n💻Usage: *.n2*\nℹ️Desc: *Birine kalp gönder bakalım*\n\n💻Usage: *.n3*\nℹ️Desc: *Tank göndermeye ne dersin ?*\n\n💻Usage: *.n4*\nℹ️Desc: *Gs aşkıyla yanıp tutuşanlar için*\n\n💻Usage: *.n5*\nℹ️Desc: *Şekilli disslike göndermeye ne dersin ?*\n\n💻Usage: *.n6*\nℹ️Desc: *Sohbeti neşelendirecek kedi göndermeye ne dersin?*\n\nCodded by *@phaticusthiccy*\nUpdated by *@shadokiller*\nVersiyon: *2.0*");
+
+}));
+
+Asena.addCommand({pattern: 'm7', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("\n♪ღ♪*•.¸¸.•*¨¨*•.♪ღ♪*•.¸¸.•*¨¨*•.♪ღ♪\n░H░A░P░P░Y░♪░B░I░R░T░H░D░A░Y░\n♪ღ♪*•.¸¸.•*¨¨*•.♪ღ♪*•.¸¸.•*¨¨*•.♪ღ♪");
+
+}));
+
+Asena.addCommand({pattern: 'm8', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("\n_/﹋\_\n(҂`_´)\n<,︻╦╤─ ҉ - -\n_/﹋\_");
+
+}));
+
+Asena.addCommand({pattern: 'm9', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("╔══╗╔╗ ♡ ♡ ♡\n╚╗╔╝║║╔═╦╦╦╔╗\n╔╝╚╗║╚╣║║║║╔╣\n╚══╝╚═╩═╩═╩═╝\nஜ۞ஜ YOU ஜ۞ஜ");
+
+}));
+
+Asena.addCommand({pattern: 'n1', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage(" ▬▬▬.◙.▬▬▬\n ═▂▄▄▓▄▄▂\n◢◤ █▀▀████▄▄▄▄◢◤\n█▄▂█ █▄███▀▀▀▀▀▀▀╬\n◥█████◤\n══╩══╩═\n ╬═╬\n ╬═╬\n ╬═╬\n ╬═╬\n ╬═╬\n ╬═╬ ☻/\n ╬═╬/▌\n ╬═╬/");
+
+}));
+
+Asena.addCommand({pattern: 'n2', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("(っ◔◡◔っ) ❤");
+
+}));
+
+Asena.addCommand({pattern: 'n3', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("  ░░░░░░███████ ]▄▄▄▄▄▄▄▄          \n▂▄▅█████████▅▄▃▂   \nIl███████████████████].   ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤..\n");
+  
+}));
+
+Asena.addCommand({pattern: 'n4', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑\n🌑🌑🌒🌘🌑🌑🌑🌒🌘🌑🌑\n🌑🌔🌕🌕🌕🌑🌕🌕🌕🌖🌑\n🌑🌕🌕🌕🌕🌕🌕🌕🌕🌕🌑\n🌑🌔🌕🌕🌕🌕🌕🌕🌕🌖🌑\n🌑🌒🌕🌕🌕🌕🌕🌕🌕🌘🌑\n🌑🌑🌒🌕🌕🌕🌕🌕🌘🌑🌑\n🌑🌑🌒🌕🌕🌕🌕🌕🌘🌑🌑\n🌑🌑🌑🌒🌕🌕🌕🌘🌑🌑🌑\n🌑🌑🌑🌑🌒🌕🌘🌑🌑🌑🌑\n🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑\n🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑\n🌑🌑🌒🌘🌑🌑🌑🌒🌘🌑🌑\n🌑🌒❤️❤️🌘🌑🌒❤️❤️🌘🌑\n🌑🌔❤️❤️🌕🌑❤️❤️❤️🌖🌑\n🌑❤️❤️❤️❤️❤️❤️❤️❤️❤️🌑\n🌑🌔❤️❤️❤️❤️❤️❤️❤️🌖🌑\n🌑🌒❤️❤️❤️❤️❤️❤️❤️🌘🌑\n🌑🌑🌒❤️❤️❤️❤️❤️🌘🌑🌑\n🌑🌑🌑🌒❤️❤️❤️🌘🌑🌑🌑\n🌑🌑🌑🌑🌒🌕🌘🌑🌑🌑🌑\n🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑🌑");
+
+}));
+
+Asena.addCommand({pattern: 'n5', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("███████▄▄███████████▄\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓███░░░░░░░░░░░░█\n██████▀░░█░░░░██████▀\n░░░░░░░░░█░░░░█\n░░░░░░░░░░█░░░█\n░░░░░░░░░░░█░░█\n░░░░░░░░░░░█░░█\n░░░░░░░░░░░░▀▀");
+
+}));
+
+Asena.addCommand({pattern: 'n6', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    await message.sendMessage("──────▄▀▄─────▄▀▄\n─────▄█░░▀▀▀▀▀░░█▄\n─▄▄──█░░░░░░░░░░░█──▄▄\n█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█");
+
+}));
